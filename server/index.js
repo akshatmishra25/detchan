f56cc614b6a5ecd5672ca318c887b05d450b7616
Akshat Mishra <akshatmishra317@gmail.com>
2024-05-06 20:44:31 +0530

    implemented socket

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authroutes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cors from "cors";
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // Specify the allowed origins
+    credentials: true, // Allow credentials
+  })
+  );
+  dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+/*app.get("/", (req, res) => {
+    res.send("Hello World!");
+});*/
+
+
+server.listen(PORT, () => {
+    connectToMongoDB();
+    
+    console.log(`Server running on port ${PORT}`)
+    });
