5fe9f27437e3f01f9b949515303133083b7f9e03
Akshat Mishra <akshatmishra317@gmail.com>
2024-05-06 20:51:37 +0530

    modified index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,6 +11,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+
+
 app.use(
   cors({
     origin: ["http://localhost:3000"], // Specify the allowed origins
@@ -18,6 +21,8 @@ app.use(
   );
   dotenv.config();
 
+  const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,6 +30,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 /*app.get("/", (req, res) => {
     res.send("Hello World!");
